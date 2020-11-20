76046597ef5ebd637f91d01a00b35e6d0dcc6b40
Milkac-Design <milkac88@gmail.com>
2020-11-20 09:00:21 +0100

    Minor CSS and function change

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,19 @@ export default function Header(props) {
   return (
     <footer className="footerStyle">
       <img className="logoStyle" src="./logo.png" alt="logo" />
+      <div className='getInTouch'>
+        <p>Get in touch: </p>
+        <Link href="https://github.com/Milkac-Design">
+          <a target="_blank">
+            <img className="footerLogo" src="/GitHub_logo.png" />
+          </a>
+        </Link>
+        <Link href="https://www.linkedin.com/in/mario-milkovic-5a904aa1/">
+          <a target="_blank">
+            <img className="footerLogo" src="/linkedIn_logo.png" />
+          </a>
+        </Link>
+      </div>
       <nav>
         <Link href="/">
           <a>Home</a>
@@ -20,10 +33,10 @@ export default function Header(props) {
             <a>Log out</a>
           </Link>
         ) : (
-            <Link href="/login">
-              <a>Log in</a>
-            </Link>
-          )}
+          <Link href="/login">
+            <a>Log in</a>
+          </Link>
+        )}
       </nav>
     </footer>
   );
