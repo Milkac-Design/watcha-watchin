dbc0d227a143c7c29619b984f9bcea1c172d3ca1
Milkac-Design <milkac88@gmail.com>
2020-11-24 15:34:55 +0100

    Fix bug, deleted font library

--- pages/mymovies.js
+++ pages/mymovies.js
@@ -20,6 +20,10 @@ export default function MyMovies(props) {
         ></link>
         <link
           rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        ></link>
+        <link
+          rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
         />
       </Head>
